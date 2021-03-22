701cca76c43c24beeebc1e6f775af329de18356e
Blarc <jakob.malezic@gmail.com>
2021-03-22 13:14:25 +0100

    added basic parallel logic

---
+++ pa1/src/main/java/wier23/callable/PageCrawl.java
@@ -0,0 +1,34 @@
+package wier23.callable;
+
+import java.util.concurrent.Callable;
+import java.util.logging.Logger;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import wier23.entity.Page;
+
+public class PageCrawl implements Callable<PageCrawl>
+{
+    private final Logger logger = Logger.getLogger(PageCrawl.class.getName());
+
+    private final ChromeDriver chromeDriver;
+    private final Page page;
+
+    public PageCrawl(Page page, ChromeDriver chromeDriver) {
+        this.chromeDriver = chromeDriver;
+        this.page = page;
+    }
+
+    @Override
+    public PageCrawl call()
+    {
+        logger.info("Visiting: " + page.getUrl());
+        chromeDriver.get(page.getUrl());
+        return this;
+    }
+
+    public ChromeDriver getChromeDriver()
+    {
+        return chromeDriver;
+    }
+}
--- pa1/src/main/java/wier23/service/CrawlManagerService.java
+++ pa1/src/main/java/wier23/service/CrawlManagerService.java
@@ -2,33 +2,33 @@ package wier23.service;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-import javax.annotation.PostConstruct;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.stereotype.Service;
 
+import wier23.callable.PageCrawl;
 import wier23.entity.Page;
 
 @Service
 public class CrawlManagerService
 {
-    public static final int THREAD_COUNT = 5;
-
     private final Logger logger = Logger.getLogger(CrawlManagerService.class.getName());
 
     private final FrontierService frontierService;
 
-    private final ChromeOptions chromeOptions;
-
     private final Queue<ChromeDriver> driverQueue;
 
     private final ExecutorService executorService;
+
+    private final Queue<Future<PageCrawl>> futureList;
 
     public CrawlManagerService(FrontierService frontierService, int threadCount)
     {
@@ -36,22 +36,46 @@ public class CrawlManagerService
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
-        chromeOptions = options;
 
         executorService = Executors.newFixedThreadPool(threadCount);
+        futureList = new ConcurrentLinkedQueue<>();
 
         driverQueue = new LinkedList<>();
         for (int i = 0; i < threadCount; i++) {
-            driverQueue.add(new ChromeDriver(chromeOptions));
+            driverQueue.add(new ChromeDriver(options));
         }
 
     }
 
     public void run() {
         Page page;
-        logger.log(Level.INFO, "Printing urls...");
-        while((page = frontierService.getNextPage()) != null) {
-            logger.log(Level.INFO, page.getUrl());
+        while((page = frontierService.getNextPage()) != null || !futureList.isEmpty()) {
+
+            if (!futureList.isEmpty()) {
+
+                Future<PageCrawl> polledFuture = futureList.poll();
+                if (polledFuture.isDone()) {
+                    try
+                    {
+                        PageCrawl pageCrawl = polledFuture.get();
+                        driverQueue.add(pageCrawl.getChromeDriver());
+                    }
+                    catch (InterruptedException | ExecutionException e)
+                    {
+                        logger.severe(e.getMessage());
+                    }
+                }
+                else {
+                    futureList.add(polledFuture);
+                }
+            }
+
+            if (page != null) {
+                PageCrawl pageCrawl = new PageCrawl(page, driverQueue.poll());
+                Future<PageCrawl> future = executorService.submit(pageCrawl);
+                futureList.add(future);
+            }
+
         }
     }
 }
